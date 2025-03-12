da2487694df2af56dfd0e4d3ed95d68d544b0880
pratiksha-katore24 <158153399+pratiksha-katore24@users.noreply.github.com>
2025-03-12 16:47:10 +0530

    Add files via upload

---
+++ array desending-order.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,temp;
+	int a[10]={101,38,56,33,21,17,11,5,2};
+	for(i=0;i<10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if(a[j]>a[i])
+			{
+				temp=a[i];
+				a[i]=a[j];			
+				a[j]=temp;
+			}
+	}
+}
+printf("printing sorting element list..");
+for(i=0;i<10;i++)
+{
+	printf("%d \n",a[i]);
+
+}
+}
