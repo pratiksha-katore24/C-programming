c24172cc8da33cc6b73b42c1a8702159be0aceb8
pratiksha-katore24 <158153399+pratiksha-katore24@users.noreply.github.com>
2025-03-12 16:51:30 +0530

    Add files via upload

---
+++ array-3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+
+int marks[5];
+marks[0]=10;
+marks[1]=20;
+marks[2]=30;
+marks[3]=40;
+marks[4]=50;
+{
+
+printf("%d\t",marks[0]);
+printf("%d\t",marks[1]);
+printf("%d\t",marks[2]);
+printf("%d\t",marks[3]);
+printf("%d\t",marks[4]);
+
+}}
